79ff6d993861be52cb0eb99cc40c63b2074d6d0b
Alfred Doh-Nani <alfred.doh-nani@meltwater.org>
2020-01-21 14:55:23 +0000

    Write data graph's resolvers

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -5,6 +5,7 @@ const resolvers = require("./resolvers");
 
 const LaunchAPI = require("./datasources/launch");
 const UserAPI = require("./datasources/user");
+const isEmail = require("isemail");
 
 const store = createStore();
 
@@ -14,7 +15,16 @@ const server = new ApolloServer({
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
     userAPI: new UserAPI({ store })
-  })
+  }),
+  context: async ({ req }) => {
+    const auth = (req.headers && req.headers.authorization) || "";
+    const email = Buffer.from(auth, "base64").toString("ascii");
+    if (!isEmail.validate(email)) return { user: null };
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = (users && users[0]) || null;
+
+    return { user: { ...user.dataValues } };
+  }
 });
 
 server
--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -35,7 +35,7 @@ const typeDefs = gql`
 
   type Mission {
     name: String
-    missionPatch(size: PatchSize): String
+    missionPatch(mission: String, size: PatchSize): String
   }
 
   enum PatchSize {
