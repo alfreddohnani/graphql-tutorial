bced5b1588d1372266c6493a9b329e97d1657e79
Alfred Doh-Nani <alfred.doh-nani@meltwater.org>
2020-01-27 11:30:39 +0000

    Fetch data with queries

--- start/client/src/pages/launch.tsx
+++ start/client/src/pages/launch.tsx
@@ -1,12 +1,54 @@
-import React from 'react';
-import { RouteComponentProps } from '@reach/router';
+import React, { Fragment } from "react";
+import { RouteComponentProps } from "@reach/router";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+import { Loading, Header, LaunchDetail } from "../components";
+import { ActionButton } from "../containers";
+import * as LaunchDetailsTypes from "./__generated__/LaunchDetails";
+
+import { LAUNCH_TILE_DATA } from "./launches";
 
 interface LaunchProps extends RouteComponentProps {
-
+  launchId?: any;
 }
 
-const Launch: React.FC<LaunchProps> = () => {
-  return <div />;
-}
+const Launch: React.FC<LaunchProps> = ({ launchId }) => {
+  const { data, loading, error } = useQuery<
+    LaunchDetailsTypes.LaunchDetails,
+    LaunchDetailsTypes.LaunchDetailsVariables
+  >(GET_LAUNCH_DETAILS, { variables: { launchId } });
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+    <Fragment>
+      <Header
+        image={
+          data.launch && data.launch.mission && data.launch.mission.missionPatch
+        }
+      >
+        {data && data.launch && data.launch.mission && data.launch.mission.name}
+      </Header>
+      <LaunchDetail {...data.launch} />
+      <ActionButton {...data.launch} />
+    </Fragment>
+  );
+};
+
+export const GET_LAUNCH_DETAILS = gql`
+  query LaunchDetails($launchId: ID!) {
+    launch(id: $launchId) {
+      site
+      rocket {
+        type
+      }
+      ...LaunchTile
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
 
 export default Launch;
--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,11 +1,91 @@
-import React, { Fragment }  from 'react';
-import { RouteComponentProps } from '@reach/router';
+import React, { Fragment } from "react";
+import { RouteComponentProps } from "@reach/router";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+import { LaunchTile, Header, Button, Loading } from "../components";
+import * as GetLaunchListTypes from "./__generated__/GetLaunchList";
+
+export const LAUNCH_TILE_DATA = gql`
+  fragment LaunchTile on Launch {
+    id
+    isBooked
+    rocket {
+      id
+      name
+    }
+    mission {
+      name
+      missionPatch
+    }
+  }
+`;
+
+const GET_LAUNCHES = gql`
+  query launchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
+
+
 
 interface LaunchesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
-  return <div />;
-}
+  const { data, loading, error, fetchMore } = useQuery<
+    GetLaunchListTypes.GetLaunchList,
+    GetLaunchListTypes.GetLaunchListVariables
+  >(GET_LAUNCHES);
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+    <Fragment>
+      <Header />
+      {data.launches &&
+        data.launches.launches &&
+        data.launches.launches.map((launch: any) => (
+          <LaunchTile key={launch.id} launch={launch} />
+        ))}
+
+      {data.launches && data.launches.hasMore && (
+        <Button
+          onClick={() =>
+            fetchMore({
+              variables: {
+                after: data.launches.cursor
+              },
+
+              updateQuery: (prev, { fetchMoreResult, ...rest }) => {
+                if (!fetchMoreResult) return prev;
+                return {
+                  ...fetchMoreResult,
+                  launches: {
+                    ...fetchMoreResult.launches,
+                    launches: [
+                      ...prev.launches.launches,
+                      ...fetchMoreResult.launches.launches
+                    ]
+                  }
+                };
+              }
+            })
+          }
+        >
+          Load More
+        </Button>
+      )}
+    </Fragment>
+  );
+};
 
 export default Launches;
-
